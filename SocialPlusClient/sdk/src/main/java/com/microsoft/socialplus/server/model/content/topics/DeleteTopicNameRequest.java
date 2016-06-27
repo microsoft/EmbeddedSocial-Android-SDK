43f343f8b58da07c09f5c5cc469ede6f8cc5d920
Alex Crown <acrown@microsoft.com>
2016-06-27 11:20:02 -0700

    Added requests for named topics

---
+++ SocialPlusClient/sdk/src/main/java/com/microsoft/socialplus/server/model/content/topics/DeleteTopicNameRequest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *
+ */
+
+package com.microsoft.socialplus.server.model.content.topics;
+
+import com.microsoft.socialplus.autorest.models.PublisherType;
+import com.microsoft.rest.ServiceException;
+import com.microsoft.rest.ServiceResponse;
+import com.microsoft.socialplus.server.exception.NetworkRequestException;
+import com.microsoft.socialplus.server.model.UserRequest;
+
+import java.io.IOException;
+
+import retrofit2.Response;
+
+public class DeleteTopicNameRequest extends UserRequest {
+
+    private final String topicName;
+    private final com.microsoft.socialplus.autorest.models.DeleteTopicNameRequest requestBody;
+
+    public DeleteTopicNameRequest(String topicName, PublisherType publisherType) {
+        this.topicName = topicName;
+        requestBody = new com.microsoft.socialplus.autorest.models.DeleteTopicNameRequest();
+        requestBody.setPublisherType(publisherType);
+    }
+
+    @Override
+    public Response send() throws NetworkRequestException {
+        ServiceResponse<Object> serviceResponse;
+        try {
+            serviceResponse = TOPICS.deleteTopicName(topicName, requestBody, bearerToken, appKey, null);
+        } catch (ServiceException|IOException e) {
+            throw new NetworkRequestException(e.getMessage());
+        }
+        checkResponseCode(serviceResponse);
+
+        return serviceResponse.getResponse();
+    }
+}
