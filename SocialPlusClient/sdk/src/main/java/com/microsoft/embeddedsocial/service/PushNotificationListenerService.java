bfd96a776751d21e353a171fd51f0aa91128ca2c
Alex Crown <acrown@microsoft.com>
2017-02-03 17:20:48 -0800

    Push notifications filtered by publisher

--- SocialPlusClient/sdk/src/main/java/com/microsoft/embeddedsocial/service/PushNotificationListenerService.java
+++ SocialPlusClient/sdk/src/main/java/com/microsoft/embeddedsocial/service/PushNotificationListenerService.java
@@ -15,14 +15,25 @@ import com.microsoft.embeddedsocial.base.utils.debug.DebugLog;
  * Is used to receive GCM push notifications.
  */
 public class PushNotificationListenerService extends GcmListenerService {
-
+	private static final String EMBEDDED_SOCIAL_PUBLISHER = "EmbeddedSocial";
 	private static final String KEY_MESSAGE_TEXT = "msg";
+	private static final String KEY_PUBLISHER = "publisher";
 
 	@Override
 	public void onMessageReceived(String from, Bundle data) {
 		super.onMessageReceived(from, data);
 		DebugLog.i("received a push message");
 		DebugLog.logBundle(data);
-		new PushNotificationReceivedEvent(data.getString(KEY_MESSAGE_TEXT, "")).submit();
+		if (isSocialNotification(data)) {
+			new PushNotificationReceivedEvent(data.getString(KEY_MESSAGE_TEXT, "")).submit();
+		}
+	}
+
+	/**
+	 * Returns true if the notification is from Embedded Social
+     */
+	public boolean isSocialNotification(Bundle data) {
+		String publisher = data.getString(KEY_PUBLISHER);
+		return EMBEDDED_SOCIAL_PUBLISHER.equals(publisher);
 	}
 }
