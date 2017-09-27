dc490ac7dcb64c71e390236edd96867489886afd
Alex Crown <acrown@microsoft.com>
2017-09-26 17:32:45 -0700

    Updating the Activity Feed to only display the following acivity feed if user relations are enabled

--- EmbeddedSocialClient/sdk/src/main/java/com/microsoft/embeddedsocial/ui/activity/RecentActivityActivity.java
+++ EmbeddedSocialClient/sdk/src/main/java/com/microsoft/embeddedsocial/ui/activity/RecentActivityActivity.java
@@ -6,7 +6,10 @@
 package com.microsoft.embeddedsocial.ui.activity;
 
 import android.support.v4.view.PagerAdapter;
+import android.telephony.gsm.GsmCellLocation;
 
+import com.microsoft.embeddedsocial.base.GlobalObjectRegistry;
+import com.microsoft.embeddedsocial.sdk.Options;
 import com.microsoft.embeddedsocial.ui.fragment.UserActivityFeedFragment;
 import com.microsoft.embeddedsocial.sdk.R;
 import com.microsoft.embeddedsocial.ui.activity.base.BaseTabsActivity;
@@ -24,10 +27,19 @@ public class RecentActivityActivity extends BaseTabsActivity {
 
 	@Override
 	protected PagerAdapter createPagerAdapter() {
-		return new SimplePagerAdapter(this, getSupportFragmentManager(),
-			new SimplePagerAdapter.Page(R.string.es_activity_feed_user, UserActivityFeedFragment::new),
-			new SimplePagerAdapter.Page(R.string.es_activity_feed_following, FollowingActivityFeedFragment::new)
-		);
+		SimplePagerAdapter.Page userFeed = new SimplePagerAdapter.Page(R.string.es_activity_feed_user, UserActivityFeedFragment::new);
+		SimplePagerAdapter.Page followingFeed = new SimplePagerAdapter.Page(R.string.es_activity_feed_following, FollowingActivityFeedFragment::new);
+
+		SimplePagerAdapter.Page[] pages;
+
+		// Only show the following activity feed if user relations are enabled
+		if (GlobalObjectRegistry.getObject(Options.class).userRelationsEnabled()) {
+			pages = new SimplePagerAdapter.Page[] {userFeed, followingFeed};
+		} else { // user relations are not enabled
+			pages = new SimplePagerAdapter.Page[] {userFeed};
+		}
+
+		return new SimplePagerAdapter(this, getSupportFragmentManager(), pages);
 	}
 
 	@Override
