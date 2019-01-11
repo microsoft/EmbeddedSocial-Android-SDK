ab1b8941bbf439f4750c5d1bc723ec20992b70ec
Alex Crown <acrown@microsoft.com>
2019-01-10 17:46:13 -0800

    Remove unused class UpdateNotificationCountHandler

--- EmbeddedSocialClient/sdk/src/main/java/com/microsoft/embeddedsocial/service/IntentProcessorFactory.java
+++ EmbeddedSocialClient/sdk/src/main/java/com/microsoft/embeddedsocial/service/IntentProcessorFactory.java
@@ -8,7 +8,6 @@ package com.microsoft.embeddedsocial.service;
 import com.microsoft.embeddedsocial.base.service.IServiceIntentProcessor;
 import com.microsoft.embeddedsocial.base.service.IntentProcessor;
 import com.microsoft.embeddedsocial.service.handler.LinkUserThirdPartyAccountHandler;
-import com.microsoft.embeddedsocial.service.handler.UpdateNotificationCountHandler;
 
 import android.content.Context;
 
@@ -28,7 +27,6 @@ public class IntentProcessorFactory {
     IServiceIntentProcessor createIntentProcessor() {
         IntentProcessor<ServiceAction> processor = new IntentProcessor<>(context, ServiceAction.class, WORKER_THREADS);
 
-        processor.registerIntentHandler(ServiceAction.UPDATE_NOTIFICATION_COUNT, new UpdateNotificationCountHandler());
         processor.registerIntentHandler(ServiceAction.LINK_USER_THIRD_PARTY_ACCOUNT, new LinkUserThirdPartyAccountHandler());
 
         return processor;
